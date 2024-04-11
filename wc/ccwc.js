0faa1b026ba1bf01bcb181b10df14d5f3e9d7c9c
Billmike <ayelegun.kayode@gmail.com>
2024-04-11 14:00:56 +0200

    chore: implement counting number of words

--- wc/ccwc.js
+++ wc/ccwc.js
@@ -5,7 +5,7 @@ function countBytes(filename) {
     const stats = fs.statSync(filename);
     return stats.size;
   } catch (error) {
-    return -1;
+    console.log(`Error reading file: ${filename}`, error);
   }
 }
 
@@ -21,14 +21,29 @@ function countLines(filename) {
 
     return numberOfLines;
   } catch (error) {
-    return -1;
+    console.log(`Error reading file: ${filename}`, error);
+  }
+}
+
+function countWords(filename) {
+  try {
+    const fileContentData = fs.readFileSync(filename, "utf8");
+    const words = fileContentData
+      .split(/\s+/)
+      .filter((word) => word.length > 0);
+    return words.length;
+  } catch (error) {
+    console.log(`Error reading file: ${filename}`, error);
   }
 }
 
 function wcClone() {
   const args = process.argv.slice(2);
 
-  if (args.length !== 2 || (args[0] !== "-c" && args[0] !== "-l")) {
+  if (
+    args.length !== 2 ||
+    (args[0] !== "-c" && args[0] !== "-l" && args[0] !== "-w")
+  ) {
     console.log("Usage: node ccwc.js -c|-l filename");
     return;
   }
@@ -36,24 +51,16 @@ function wcClone() {
   const option = args[0];
   const filename = args[1];
 
-  if (option === "-l") {
-    const lineCount = countLines(filename);
-
-    if (lineCount === -1) {
-      console.error(`Error reading file: ${filename}`);
-      process.exit(1);
-    }
-
-    console.log(`${lineCount} ${filename}`);
-  } else if (option === "-c") {
-    const byteCount = countBytes(filename);
-
-    if (byteCount === -1) {
-      console.error(`Error reading file: ${filename}`);
-      process.exit(1);
-    }
-
-    console.log(`${byteCount} ${filename}`);
+  switch (option) {
+    case "-c":
+      console.log(`${countBytes(filename)} ${filename}`);
+      break;
+    case "-l":
+      console.log(`${countLines(filename)} ${filename}`);
+      break;
+    case "-w":
+      console.log(`${countWords(filename)} ${filename}`);
+      break;
   }
 }
 
